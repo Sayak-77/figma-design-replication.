e28bd9af8d9cfd748b0f8e23359bd1413a427237
dragneel <sayakascent@gmail.com>
2024-06-29 10:05:06 +0530

    uptodd

---
+++ client/src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../styles/mainstyles.css';
+const Main = () => {
+  return (
+    <div>
+      <div className='outro'>
+        <div className='ach'>Achievers</div>
+        <div className='real'>RealStories: Watch Babies Thrive</div>
+      </div>
+    </div>
+  )
+}
+
+export default Main
