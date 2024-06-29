712e5f9fd59e1535acd16a0aa4a0c1532b2c60b2
dragneel <sayakascent@gmail.com>
2024-06-29 11:17:58 +0530

    new

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Main from './components/Main';
+import Modalform from './components/Modalform';
 
 function App() {
   return (
     <>
     <Navbar/>
     <Main/>
+    <Modalform/>
     </>
   );
 }
